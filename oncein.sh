ea146a6b3afff893e8ccc3864d18e754d98c78dd
Ian Bissett <bisscuitt@gmail.com>
2013-04-03 13:25:03 +1200

    Update oncein.sh
    Make sure that we check the age of the lockfile before the call to flock. 
    
    Otherwise we sleep the first time the command is ran.
    
    Added a `which` lookup so shorthand can be used for $cmd

--- oncein.sh
+++ oncein.sh
@@ -2,7 +2,7 @@
 
 seconds=$1
 shift
-cmd=$1
+cmd=`which $1`
 shift
 
 if [ x$seconds == 'x'  ] || [ x$cmd == 'x' ] || [ ! -x $cmd ] ; then
@@ -12,22 +12,27 @@ fi
 
 timestampfile="/var/lock/oncein_`basename $cmd`.lock"
 
+# Check the age of the lockfile
+if [ -f $timestampfile ] ; then
+	age=$(( $(date +"%s") - $(stat -c "%Y" $timestampfile) ))
+else
+  # We wont sleep at all if the lockfile does not yet exist
+	age=$seconds
+fi
+
 # Get an exclusive lock
 exec 9>>$timestampfile
 flock 9
 {
-	# seconds since lockfile was last touched
-	age=$(( $(date +"%s") - $(stat -c "%Y" $timestampfile) ))
-
 	# sleep if necessary to make up the correct number of seconds
 	if [ $age -lt $seconds ] ; then
 		sleep $(($seconds - $age))
 	fi
-	
-	# we're done sleeping. update the timestamp before releasing the lock
+
+	# Update the timestamp before releasing the lock
 	touch $timestampfile
 
-# release the lock		
+# release the lock
 } 9<&-
 exec 9<&-
 
