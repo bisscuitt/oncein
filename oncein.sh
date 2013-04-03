5d7e11116e93debdef50fbb6bc578ac7ccaad240
Ian Bissett <bisscuitt@gmail.com>
2013-04-03 15:55:14 +1200

    Update oncein.sh
    Using a flag to ensure we don't sleep on the first run.

--- oncein.sh
+++ oncein.sh
@@ -2,32 +2,38 @@
 
 seconds=$1
 shift
-cmd=$1
+cmd=`which $1`
 shift
 
 if [ x$seconds == 'x'  ] || [ x$cmd == 'x' ] || [ ! -x $cmd ] ; then
-	echo 'Usage: oncein <seconds> <command> [args..]'
-	exit 3
+    echo 'Usage: oncein <seconds> <command> [args..]'
+    exit 3
 fi
 
 timestampfile="/var/lock/oncein_`basename $cmd`.lock"
+firstrun=0
+if [ ! -f $timestampfile ] ; then
+    firstrun=1
+fi
 
 # Get an exclusive lock
 exec 9>>$timestampfile
 flock 9
 {
-	# seconds since lockfile was last touched
-	age=$(( $(date +"%s") - $(stat -c "%Y" $timestampfile) ))
+    if [ ! "$firstrun" -eq "1" ] ; then
+        # seconds since lockfile was last touched
+        age=$(( $(date +"%s") - $(stat -c "%Y" $timestampfile) ))
 
-	# sleep if necessary to make up the correct number of seconds
-	if [ $age -lt $seconds ] ; then
-		sleep $(($seconds - $age))
-	fi
-	
-	# we're done sleeping. update the timestamp before releasing the lock
-	touch $timestampfile
+        # sleep if necessary to make up the correct number of seconds
+        if [ $age -lt $seconds ] ; then
+            sleep $(($seconds - $age))
+        fi
+    fi
 
-# release the lock		
+    # we're done sleeping. update the timestamp before releasing the lock
+    touch $timestampfile
+
+# release the lock      
 } 9<&-
 exec 9<&-
 
