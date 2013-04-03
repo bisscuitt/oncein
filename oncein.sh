df314ad42322f766beb06d413721ce4c0b330c12
Ian Bissett <bisscuitt@gmail.com>
2013-04-03 15:59:22 +1200

    Update oncein.sh
    fix whitespace

--- oncein.sh
+++ oncein.sh
@@ -6,32 +6,32 @@ cmd=`which $1`
 shift
 
 if [ x$seconds == 'x'  ] || [ x$cmd == 'x' ] || [ ! -x $cmd ] ; then
-    echo 'Usage: oncein <seconds> <command> [args..]'
-    exit 3
+	echo 'Usage: oncein <seconds> <command> [args..]'
+	exit 3
 fi
 
 timestampfile="/var/lock/oncein_`basename $cmd`.lock"
 firstrun=0
 if [ ! -f $timestampfile ] ; then
-    firstrun=1
+	firstrun=1
 fi
 
 # Get an exclusive lock
 exec 9>>$timestampfile
 flock 9
 {
-    if [ ! "$firstrun" -eq "1" ] ; then
-        # seconds since lockfile was last touched
-        age=$(( $(date +"%s") - $(stat -c "%Y" $timestampfile) ))
+	if [ ! "$firstrun" -eq "1" ] ; then
+		# seconds since lockfile was last touched
+		age=$(( $(date +"%s") - $(stat -c "%Y" $timestampfile) ))
 
-        # sleep if necessary to make up the correct number of seconds
-        if [ $age -lt $seconds ] ; then
-            sleep $(($seconds - $age))
-        fi
-    fi
+		# sleep if necessary to make up the correct number of seconds
+		if [ $age -lt $seconds ] ; then
+			sleep $(($seconds - $age))
+		fi
+	fi
 
-    # we're done sleeping. update the timestamp before releasing the lock
-    touch $timestampfile
+	# we're done sleeping. update the timestamp before releasing the lock
+	touch $timestampfile
 
 # release the lock      
 } 9<&-
